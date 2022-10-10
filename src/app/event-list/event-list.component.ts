bc45f541590bc0d20bcc41a09f305430936ce033
Peng Mong <pengmnt@gmail.com>
2022-10-09 23:52:32 -0500

    sorting events by date range (may still be WIP), more testing required

--- src/app/event-list/event-list.component.ts
+++ src/app/event-list/event-list.component.ts
@@ -12,25 +12,36 @@ import { IEvent } from '../interfaces/IEvent';
 export class EventListComponent implements OnInit {
 
   isEventList: boolean = true;
+  from!: string;
+  to!: string;
   private user: IAccount;
   private eventList!: IEvent[];
   private inviteList!: IEvent[];
+  private eventListCopy!: IEvent[];
+  private inviteListCopy!: IEvent[];
   sub: Subscription;
 
   constructor(private data: DataService) {
     this.user = this.data.getUser();
-    this.eventList = this.user.events.sort(function(a, b){return Date.parse(a.date) - Date.parse(b.date)});
-    this.inviteList = this.user.invitations.sort(function(a, b){return Date.parse(a.date) - Date.parse(b.date)});
+    this.setLists();
+    this.eventListCopy = [...this.eventList];
+    this.inviteListCopy = [...this.inviteList];
     this.sub = this.data.$user.subscribe((user) => {
       this.user = user;
       this.eventList = this.user.events.sort(function(a, b){return Date.parse(a.date) - Date.parse(b.date)});
       this.inviteList = this.user.invitations.sort(function(a, b){return Date.parse(a.date) - Date.parse(b.date)});
+      this.eventListCopy = [...this.eventList];
+      this.inviteListCopy = [...this.inviteList];
     });
   }
 
   ngOnInit(): void {
   }
 
+  setLists(){
+    this.eventList = this.user.events.sort(function(a, b){return Date.parse(a.date) - Date.parse(b.date)});
+    this.inviteList = this.user.invitations.sort(function(a, b){return Date.parse(a.date) - Date.parse(b.date)});
+  }
   getUser(){
     return this.user;
   }
@@ -41,5 +52,24 @@ export class EventListComponent implements OnInit {
 
   getInvitationsList(){
     return this.inviteList;
+  }
+
+  filter(){
+    if(this.isEventList === true){
+      this.eventList = [];
+      for(let event of this.eventListCopy){
+        if(Date.parse(event.date) >= Date.parse(this.from) && Date.parse(event.date) <= Date.parse(this.to)){
+          this.eventList.push(event);
+        }
+      }
+    }else{
+      this.inviteList = [];
+      for(let event of this.inviteListCopy){
+        if(Date.parse(event.date) >= Date.parse(this.from) && Date.parse(event.date) <= Date.parse(this.to)){
+          this.inviteList.push(event);
+        }
+      }
+    }
+    console.log(this.eventList);
   }
 }
